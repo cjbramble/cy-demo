dca8908400c6c12d1a243c949928d91598939073
cjbramble <email@chetbramble.com>
2022-01-10 19:32:37 -0500

    separate tests

--- cypress/integration/mock-response.spec.js
+++ cypress/integration/mock-response.spec.js
@@ -10,11 +10,29 @@ const description = lorem.paragraph();
 
 describe("Can submit message", () => {
     it("Fills in and submits message", () => {
-        cy.visit("");
+        cy.visit("/");
+
         cy.get("#name").type(firstName + " " + lastName);
         cy.get("#email").type(exampleEmail);
         cy.get("#phone").type(phoneNumber);
         cy.get("#subject").type(subject);
         cy.get("#description").type(description);
+        cy.get("#submitContact").click();
+        cy.get('div > [style="font-weight: bold;"]').should(
+            "have.text",
+            subject
+        );
+    });
+
+    it("Displays stubbed response", () => {
+        cy.intercept("POST", "https://automationintesting.online/#/message/", {
+            fixture: "message_payload.json",
+        }).as("postMessage");
+
+        cy.visit("/");
+
+        cy.wait("@postMessage").then((interception) => {
+            cy.get("#submitContact").click();
+        });
     });
 });
